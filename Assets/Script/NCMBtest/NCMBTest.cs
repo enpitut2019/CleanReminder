9f7e3812ab2a6bc44c393f238bcea35da6717a45
seiji <blueg0826@gmail.com>
2019-11-18 20:07:48 +0900

    NBMCの導入
    ・NBMCの導入をした
    ・jsonが読み込めないときの対策をした

---
+++ Assets/Script/NCMBtest/NCMBTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NCMB;
+
+public class NCMBTest : MonoBehaviour
+{
+    NCMBObject testClass = new NCMBObject("TestClass");
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        testClass["message"] = "Hello,ddd";
+        testClass.SaveAsync();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
