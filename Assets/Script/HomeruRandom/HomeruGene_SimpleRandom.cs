a66675ff7cc427ec7a2320eef26fbf1bee6419d6
SaitoSeiji <52223899+SaitoSeiji@users.noreply.github.com>
2020-02-11 14:48:10 +0900

    乱数の実装

---
+++ Assets/Script/HomeruRandom/HomeruGene_SimpleRandom.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//ただの乱数で選択
+[System.Serializable]
+public class HomeruGene_SimpleRandom : AbstractHomeru_randomGenerator
+{
+    public override string GetRandomWord()
+    {
+        float rand = Random.Range(0, 100f);
+        float unit = 100f / _wordList.Count;
+
+        for(int unitCount = 0; unitCount < _wordList.Count; unitCount++)
+        {
+            if (rand < unit * (unitCount+1))
+            {
+                return _wordList[unitCount];
+            }
+        }
+        return "";
+    }
+}
